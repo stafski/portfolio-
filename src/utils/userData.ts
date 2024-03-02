49f8ebb9b609e54ab94031c2bc98f1dcf39521a8
Leo Stafski <leo.stafski@gmail.com>
2024-03-02 00:29:25 -0300

    fix:resume language

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -9,5 +9,5 @@ export const userData = {
   about:
     "Hello, my name is Leonardo Stafski and I am a Full Stack Developer with experience developing user-friendly websites and web applications. I specialize in the Front-End and Back-End aspects of web development. On the back-end side, I excel at building robust server-side systems and databases to support the interactive and dynamic features of web applications. My skills include working with technologies like Node.js, Python, and databases like PostgreSQL. I'm dedicated to creating a seamless user experience and ensuring the functionality and performance of every project I take on.",
   resume:
-    "https://drive.google.com/file/d/1txrwJA1SGAslujSjJuse_TcN82gHu8yM/view?usp=sharing",
+    "https://drive.google.com/file/d/1FY4k1OUM1dZeh7pVhLKnla3uClDN3Xuu/view?usp=sharing",
 };
